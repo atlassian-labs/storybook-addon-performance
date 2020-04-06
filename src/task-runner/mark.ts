09fadceaafe6f9ea9accdb68ffd90dc08fae49e0
Alex Reardon <areardon@atlassian.com>
2020-04-06 16:54:04 +1000

    adding comment to mark

--- src/task-runner/mark.ts
+++ src/task-runner/mark.ts
@@ -3,6 +3,8 @@ import { addonKey } from './../addon-constants';
 const startMark: string = `storybook-addon-${addonKey}-start`;
 const endMark: string = `storybook-addon-${addonKey}-end`;
 
+// This function adds a nice marker to performance dev tools
+// It allows people to narrow down on the work done in a specific task
 export default async function mark<T>(taskName: string, fn: () => Promise<T>): Promise<T> {
   performance.mark(startMark);
   const result: T = await fn();

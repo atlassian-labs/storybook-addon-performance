1ac5dd81e64e14b1e94a9e58f191a83fa44555cc
Alex Reardon <areardon@atlassian.com>
2020-04-08 13:53:02 +1000

    trying to get ts working

---
+++ cypress/integration/run-all.spec.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('google search', () => {
+  it('should work', () => {
+    cy.visit('http://www.google.com');
+    cy.get('#lst-ib').type('Hello world{enter}');
+  });
+});

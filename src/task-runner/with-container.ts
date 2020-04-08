718df2ac5b0bfb18bcaefc0033f05593da3cade2
Alex Reardon <areardon@atlassian.com>
2020-04-08 22:13:01 +1000

    adding some comments

--- src/task-runner/with-container.ts
+++ src/task-runner/with-container.ts
@@ -3,8 +3,11 @@ export default async function withContainer<T>(
   fn: (container: HTMLElement) => Promise<T>,
 ): Promise<T> {
   const container: HTMLElement = document.createElement('div');
+  // So we can find the element
   container.setAttribute(`data-${packageName}-managed-container`, 'true');
+  // Preventing any awkward flashing for users
   container.style.visibility = 'invisible';
+
   document.body.appendChild(container);
 
   const result: T = await fn(container);

48b0e6686f8edaed469143ef42cb0fef437d6b5f
Alex Reardon <areardon@atlassian.com>
2020-04-16 14:58:30 +1000

    correctly setting timeout for custom guard

--- cypress/custom/guards.ts
+++ cypress/custom/guards.ts
@@ -3,9 +3,13 @@ function guard({ isEnabled, timeout }: { isEnabled: boolean; timeout?: number })
   // use the default timeout if none is provided
   const params = timeout != null ? { timeout } : undefined;
 
-  cy.get('@startAllButton').should(value, params);
-  cy.get('@copySelect').should(value, params);
-  cy.get('@sampleSelect').should(value, params);
+  cy.log('custom guard start');
+  // https://docs.cypress.io/api/commands/should.html#Timeouts
+  // timeout here will be passed down to the '.should()'
+  cy.get('@startAllButton', params).should(value);
+  cy.get('@copySelect', params).should(value);
+  cy.get('@sampleSelect', params).should(value);
+  cy.log('custom guard end');
 }
 
 export const instant = {

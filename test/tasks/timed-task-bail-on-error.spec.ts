66e6b0b2b0ba2972423e2e6a26c79be6051e1314
Alex Reardon <areardon@atlassian.com>
2020-04-17 13:03:19 +1000

    adding test to ensure timed tasks bail early if there is an error

---
+++ test/tasks/timed-task-bail-on-error.spec.ts
@@ -0,0 +1,27 @@
+import { runOneTimed, RunOneTimedTaskArgs } from '../../src/task-runner';
+import { ErrorResult, TimedResult, TimedTask } from '../../src/types';
+
+it('should not repeat a timed task if an error occurred in one', async () => {
+  const mock = jest.fn();
+  const task: TimedTask = {
+    type: 'timed',
+    name: 'task',
+    description: '',
+    taskId: 'task',
+    run: async (): Promise<void> => {
+      mock();
+      throw new Error('boom');
+    },
+  };
+
+  const result: TimedResult | ErrorResult = await runOneTimed({
+    task,
+    getNode: () => 'hi',
+    samples: 10,
+    copies: 1,
+  });
+
+  expect(result.type).toBe('error');
+  // after the first throw we should not continue to run the task
+  expect(mock).toHaveBeenCalledTimes(1);
+});

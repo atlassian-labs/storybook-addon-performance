d017b177b2429a3e08c6749b461520ef2897f8d4
Alex Reardon <areardon@atlassian.com>
2020-04-20 06:39:09 +1000

    adding some tests

---
+++ test/tasks/mark.spec.ts
@@ -0,0 +1,54 @@
+import { UnsupportedError } from '../../src/task-runner/custom-errors';
+import { runOneStatic, runOneTimed } from '../../src/task-runner';
+import { ErrorResult, StaticResult, StaticTask, TimedTask, TimedResult } from '../../src/types';
+
+beforeEach(() => {
+  performance.mark = jest.fn();
+  performance.measure = jest.fn();
+});
+
+it('should wrap a task in a performance.mark', async () => {
+  const task: StaticTask = {
+    taskId: 'task',
+    type: 'static',
+    description: 'task',
+    name: 'task',
+    run: async () => {
+      return 'hey';
+    },
+  };
+  expect(performance.mark).not.toHaveBeenCalled();
+  expect(performance.measure).not.toHaveBeenCalled();
+
+  const results: StaticResult | ErrorResult = await runOneStatic({
+    task,
+    getNode: () => null,
+    copies: 1,
+  });
+  expect(results.type).toEqual('static');
+  expect(performance.mark).toHaveBeenCalledTimes(2);
+  expect(performance.measure).toHaveBeenCalledTimes(1);
+});
+
+it('should still use performance.mark even when there is an error in the task', async () => {
+  const task: StaticTask = {
+    taskId: 'task',
+    type: 'static',
+    description: 'task',
+    name: 'task',
+    run: async () => {
+      throw new Error('boom');
+    },
+  };
+  expect(performance.mark).not.toHaveBeenCalled();
+  expect(performance.measure).not.toHaveBeenCalled();
+
+  const results: StaticResult | ErrorResult = await runOneStatic({
+    task,
+    getNode: () => null,
+    copies: 1,
+  });
+  expect(results.type).toEqual('error');
+  expect(performance.mark).toHaveBeenCalledTimes(2);
+  expect(performance.measure).toHaveBeenCalledTimes(1);
+});

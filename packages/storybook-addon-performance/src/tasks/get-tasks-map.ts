b1d1695c2de0b785d4c1d8581e90fa9aacc23c1a
Alex Hinds <ahinds@atlassian.com>
2023-02-20 13:57:18 +1100

    chore: use flatMap

--- packages/storybook-addon-performance/src/tasks/get-tasks-map.ts
+++ packages/storybook-addon-performance/src/tasks/get-tasks-map.ts
@@ -1,9 +1,10 @@
 import { Task, TaskGroup, TaskMap } from '../types';
-import { flatten } from 'xstate/lib/utils.js';
 
 export default function getTaskMap(groups: TaskGroup[]): TaskMap {
-  return flatten(groups.map((group) => group.tasks)).reduce((acc: TaskMap, task: Task) => {
-    acc[task.name] = task;
-    return acc;
-  }, {});
+  return groups
+    .flatMap((group) => group.tasks)
+    .reduce((acc: TaskMap, task: Task) => {
+      acc[task.name] = task;
+      return acc;
+    }, {});
 }

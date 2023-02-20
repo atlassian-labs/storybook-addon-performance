3baac684222d2bc2d96c372e2e26022a1c9239ac
Alex Hinds <ahinds@atlassian.com>
2023-02-20 13:53:27 +1100

    chore: change to utils.js

--- packages/storybook-addon-performance/src/tasks/get-tasks-map.ts
+++ packages/storybook-addon-performance/src/tasks/get-tasks-map.ts
@@ -1,5 +1,5 @@
 import { Task, TaskGroup, TaskMap } from '../types';
-import { flatten } from 'xstate/lib/utils';
+import { flatten } from 'xstate/lib/utils.js';
 
 export default function getTaskMap(groups: TaskGroup[]): TaskMap {
   return flatten(groups.map((group) => group.tasks)).reduce((acc: TaskMap, task: Task) => {
